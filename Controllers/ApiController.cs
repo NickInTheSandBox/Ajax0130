7bc91951af37db17fa3da84e5cf8443c05a972a5
NickInTheSandBox <154652330+NickInTheSandBox@users.noreply.github.com>
2024-01-30 17:54:50 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,54 @@
+﻿using Ajax.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using System.Text;
+
+namespace Ajax.Controllers
+{
+    public class ApiController : Controller
+    {
+        MyDBContext _context = new MyDBContext();
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello,World!</h1>","text/html",Encoding.UTF8);
+        }
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(p => p.City).Distinct();
+            return Json(cities);
+        }
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _context.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                if (img != null)
+                {
+                    return File(img, "image/jpeg");
+                }
+            }
+            return NotFound();
+        }
+        public IActionResult Fetch() 
+        {
+            return View();
+        }
+        public IActionResult Register(string name,int age=25)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "guest";
+            }
+            return Content($"Hello {name}. {age}歲","text/explain",Encoding.UTF8);
+        }
+        public IActionResult CheckAccount(string name)
+        {
+            if (_context.Members.Count(n=>n.Name ==name) !=0)
+            {
+                return Content("名稱已存在");
+            }
+            else return Content("OK");
+        }
+    }
+}
