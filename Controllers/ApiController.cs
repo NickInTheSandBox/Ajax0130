ce675e075d77c57609f3552aa95d3cd21f8be5ea
NickInTheSandBox <154652330+NickInTheSandBox@users.noreply.github.com>
2024-01-31 12:06:36 +0800

    0131Updae

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -17,6 +17,16 @@ namespace Ajax.Controllers
             var cities = _context.Addresses.Select(p => p.City).Distinct();
             return Json(cities);
         }
+        public IActionResult Sites(string city)
+        {
+            var sites = _context.Addresses.Where(p=>p.City==city).Select(p => p.SiteId).Distinct();
+            return Json(sites);
+        }
+        public IActionResult Roads(string site)
+        {
+            var roads = _context.Addresses.Where(p => p.SiteId == site).Select(p => p.Road).Distinct();
+            return Json(roads);
+        }
         public IActionResult Avatar(int id = 1)
         {
             Member? member = _context.Members.Find(id);
@@ -34,13 +44,13 @@ namespace Ajax.Controllers
         {
             return View();
         }
-        public IActionResult Register(string name,int age=25)
+        public IActionResult Register(string name,string email, int age=25)
         {
             if (string.IsNullOrEmpty(name))
             {
                 name = "guest";
             }
-            return Content($"Hello {name}. {age}歲","text/explain",Encoding.UTF8);
+            return Content($"Hello {name}. {age}歲. email:{email}","text/explain",Encoding.UTF8);
         }
         public IActionResult CheckAccount(string name)
         {
@@ -49,6 +59,11 @@ namespace Ajax.Controllers
                 return Content("名稱已存在");
             }
             else return Content("OK");
+        }
+        public IActionResult imgId()
+        {
+            var memID = _context.Members.Select(p => p.MemberId);
+            return Json(memID);
         }
     }
 }
